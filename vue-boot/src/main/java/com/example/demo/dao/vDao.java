adc3e2d9dea23197e5d8443ed28fdc4a4326e618
Seokhyeongyu <68406286+SeokHyeongyu@users.noreply.github.com>
2021-03-31 16:53:36 +0900

    Update vDao.java

--- vue-boot/src/main/java/com/example/demo/dao/vDao.java
+++ vue-boot/src/main/java/com/example/demo/dao/vDao.java
@@ -1,6 +1,7 @@
 package com.example.demo.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,6 @@ import org.springframework.stereotype.Component;
 @Mapper
 public interface vDao {
 	
-	List getData() throws Exception;
+	List getData(Map<String, Object> map) throws Exception;
 
 }
