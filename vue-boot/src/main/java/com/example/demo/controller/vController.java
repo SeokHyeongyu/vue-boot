e51f23496b25eae383cb9b4a188218adcf7f8e88
Seokhyeongyu <68406286+SeokHyeongyu@users.noreply.github.com>
2021-04-02 15:25:24 +0900

    Update vController.java

--- vue-boot/src/main/java/com/example/demo/controller/vController.java
+++ vue-boot/src/main/java/com/example/demo/controller/vController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.dao.vDao;
 
 @Controller
-@MapperScan(basePackages = "com.example.demo.dao")
+@MapperScan(basePackages = "com.example.demo.dao") //dao경로설정하면 mapper스캔한다.
 public class vController {
 	
 	@Autowired
@@ -22,7 +23,15 @@ public class vController {
 	
 	@ResponseBody
 	@RequestMapping(value="/getData")
-	public List<Map> getData(@RequestParam Map<String, Object> map) throws Exception {
+	public List<Map<String, Object>> getData(@RequestParam Map<String, Object> map) throws Exception {
 		return vdao.getData(map);
+	}
+	
+	//무한스크롤
+	@ResponseBody
+	@RequestMapping(value="/getScorllPage/{limit}")
+	public List<Map<String, Object>> getScorllPage(@PathVariable int limit)throws Exception {
+		return vdao.getScorllPage(limit);
+		
 	}
 }
